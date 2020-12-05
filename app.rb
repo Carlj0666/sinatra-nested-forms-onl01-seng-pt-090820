21e6749118ee0da11c17cf4cea59c7f21b5036c9
Carlj0666 <carlj1881@yahoo.com>
2020-12-05 14:21:17 -0800

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,26 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab!"
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+
+    #ISSUES FROM HERE, PARAMS LOOK GOOD, RETURNED INFO NOT CORRECT? CHECK PRY
+    post '/pirates' do
+
+      @pirate = Pirate.new(params[:name], params[:weight], params[:height])
+
+      params[:ships].each do |ship|
+        Ship.new(params[:name], params[:type], params[:booty])
+      end
+
+      erb :"pirates/show"
+    end
 
   end
 end
